de63ba20a30ed4b5710ce6e5815e70583855feef
bettet01 <bettet01@luther.edu>
2020-03-09 10:09:14 -0500

    can currently view a board

--- BattleShip/src/main/java/com/adonahue/battleship/controller/BattleShipController.java
+++ BattleShip/src/main/java/com/adonahue/battleship/controller/BattleShipController.java
@@ -16,22 +16,34 @@ public class BattleShipController {
     }
 
     public void execute() {
+        boolean gameOn = true;
         Board p1Board = new Board();
         Board p2Board = new Board();
         boolean p1Turn = true;
 
-        try {
-            view.displayBeginBanner();
-            String p1Name = view.getName();
-            view.printTurn(); //Print the current player's turn
-            for (Ship s : p1Board.getShips()) {
-                p1Board.addShip(view.placeShip(p1Board.getShips()));
-            }
-            view.printTurn();
-            for (Ship s : p2Board.getShips()) {
-                p2Board.addShip(view.placeShip(p1Board.getShips()));
-            }
+        // try {
+        //     String p1Name = view.getName();
+        //     view.printTurn(); //Print the current player's turn
+        //     for (Ship s : p1Board.getShips()) {
+        //         p1Board.addShip(view.placeShip(p1Board.getShips()));
+        //     }
+        //     view.printTurn();
+        //     for (Ship s : p2Board.getShips()) {
+        //         p2Board.addShip(view.placeShip(p1Board.getShips()));
+        //     }
 
+        // } catch(Exception e){
+        //     System.err.println("Ooops " + e );
+        // }
+
+
+        while(gameOn){
+            view.displayBeginBanner();
+            view.displayBoard(p1Board);
+            gameOn = false;
         }
+
+
+
     }
 }
