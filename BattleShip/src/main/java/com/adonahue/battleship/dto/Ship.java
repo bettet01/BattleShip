0223199de3b72ec6be23cf00334dba81eed66844
Nate Wood <nathaniel.m.wood@lawrence.edu>
2020-03-09 14:09:15 -0500

    me first

--- BattleShip/src/main/java/com/adonahue/battleship/dto/Ship.java
+++ BattleShip/src/main/java/com/adonahue/battleship/dto/Ship.java
@@ -17,11 +17,11 @@ public class Ship {
 
     // TESTING
     // public static void main(String args[]) {
-    //     Ship ship = new Ship("battleship", 4);
-    //     ArrayList<Integer> list = new ArrayList<>();
-    //     list.add(0);
-    //     list.add(1);
-    //     ship.setPosition(list, "h");
+    // Ship ship = new Ship("battleship", 4);
+    // ArrayList<Integer> list = new ArrayList<>();
+    // list.add(0);
+    // list.add(1);
+    // ship.setPosition(list, "h");
     // }
 
     public Ship(String name, int length) {
@@ -52,36 +52,35 @@ public class Ship {
     public void setPosition(HashMap<Integer, ArrayList<Integer>> position) {
         this.position = position;
     }
-    
-    
 
-    //takes in ArrayList with xy coord and orientation, sets position based off input
-    public void setNewPosition(ArrayList<Integer> location, String orientation) throws BadPlacementException{
+    // takes in ArrayList with xy coord and orientation, sets position based off
+    // input
+    public void setNewPosition(ArrayList<Integer> location, String orientation) throws BadPlacementException {
         switch (orientation) {
             case "h":
                 if (location.get(0) + this.length > 9) {
                     throw new BadPlacementException("That ship falls off the board");
-            }
+                }
                 this.position.put(0, location);
                 for (int i = 1; i < this.length; i++) {
-                    ArrayList<Integer> newLocation  = new ArrayList<>();
-                    newLocation.add(location.get(0));
-                    newLocation.add(location.get(0)+i);
-                   
+                    ArrayList<Integer> newLocation = new ArrayList<>();
+                    newLocation.add(location.get(0)+ i);
+                    newLocation.add(location.get(1));
+
                     this.position.put(i, newLocation);
                 }
                 break;
 
-                case "v":
+            case "v":
                 if (location.get(1) + this.length > 9) {
                     throw new BadPlacementException("That ship falls off the board");
-            }
+                }
                 this.position.put(0, location);
                 for (int i = 1; i < this.length; i++) {
-                    ArrayList<Integer> newLocation  = new ArrayList<>();
-                    newLocation.add(location.get(0)+i);
+                    ArrayList<Integer> newLocation = new ArrayList<>();
                     newLocation.add(location.get(0));
-                   
+                    newLocation.add(location.get(1) + i);
+
                     this.position.put(i, newLocation);
                 }
                 break;
@@ -90,5 +89,5 @@ public class Ship {
                 break;
         }
     }
-    
+
 }
